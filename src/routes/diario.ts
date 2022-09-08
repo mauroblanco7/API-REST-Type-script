e40f9298c9c6d6b5b0d6b80c64a9487c760c2638
mauroblanco7 <mauro.blnc.mb@gmail.com>
2022-09-08 20:55:20 -0300

    get by id

--- src/routes/diario.ts
+++ src/routes/diario.ts
@@ -1,9 +1,17 @@
 import express from 'express' // es6 modules se compilan como commonjs
 // commonjs : const express= require "express"
+import * as diaryServices from '../services/_diarios'
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-  res.send('diarios obtenidos')
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
+router.get('/:id', (req, res) => {
+  // + = UnaryOperator, hay que cambiar el dato a string por que asi vienen en los params
+  const diaryId = diaryServices.findById(+req.params.id)
+  return (diaryId != null)
+    ? res.send(diaryId)
+    : res.sendStatus(404)
 })
 
 router.post('/', (_req, res) => {
--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,8 @@ export interface DiaryEntry {
   visibility: Visibility
   comment: string
 }
+// PARA REUTILIZAR DATOS USAR LO SEGUIENTE
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+// Pick nos permite elegir una interface, guardarla en una variable y elegir los datos que nos interesan
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+// Omit hace que omita un dato
